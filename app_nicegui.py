46a9fd4eecf18f30b7eb34f89d218fea1899bd97
Gabriele CHIGNOLI <gabrielechignoli@icloud.com>
2026-01-12 00:47:29 +0100

    Add plotly element inline with input data area

--- app_nicegui.py
+++ app_nicegui.py
@@ -5,9 +5,18 @@
 ####
 #####
 
+"""
+TODO
+
+create tabs for plotly elements to switch between date distance speed time
+add calories computation for the json
+add calories to the plotly tabs
+"""
+
 from nicegui import ui
 from statistics import mean
 import json
+import plotly.graph_objects as go
 
 ## GLOBALS
 with open("bike_data.json") as j:
@@ -24,22 +33,27 @@ def send_data_to_json() -> None:
     ui.notify(f"Data for {date.value} added to the database")
 
 # UI Elements --------------------
-with ui.card():
-    date = ui.date_input("Day")
+with ui.row(wrap=False):
+    with ui.card():
+        date = ui.date_input("Day")
 
-    with ui.row():
-        distance = ui.number(label="km", 
-                             value=mean(bike_data_dict['distance']), 
-                             precision=2)
-        speed = ui.number(label="avg km/h", 
-                          value=mean(bike_data_dict['speed']), 
-                          precision=2)
-        time = ui.number(label="minutes", 
-                         value=mean(bike_data_dict['date']))
+        with ui.row():
+            distance = ui.number(label="km", 
+                                 value=mean(bike_data_dict['distance']), 
+                                 precision=2)
+            speed = ui.number(label="avg km/h", 
+                              value=mean(bike_data_dict['speed']), 
+                              precision=2)
+            time = ui.number(label="minutes", 
+                             value=mean(bike_data_dict['time']))
 
-ui.button("Add data", 
-          on_click=send_data_to_json, 
-          icon='directions_bike')
+        ui.button("Add data", 
+                  on_click=send_data_to_json, 
+                  icon='directions_bike')
+    fig = go.Figure(go.Scatter(x=bike_data_dict['date'], y=bike_data_dict['distance']))
+    fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+    ui.plotly(fig).classes('w-full h-80')
+
 
 # App run --------------------
 ui.run(port=1620, 
